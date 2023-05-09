e40a3ac67cb852d71f8d47309c28668252eacb17
vuonghoang3019 <vuonghk1@gmail.com>
2023-05-09 22:16:39 +0700

    update code

--- src/views/modules/about/views/Index.vue
+++ src/views/modules/about/views/Index.vue
@@ -28,28 +28,25 @@
         </template>
 
         <template #contents>
-            <easy-data-table
-                    :headers="tables.columns"
-                    :items="tables.data"
-                    theme-color="#1d90ff"
-                    :hide-footer="true"
-                    header-text-direction="center"
-                    v-model:items-selected="itemsSelected"
-                    body-text-direction="center">
-                <template #header-player="header">
-                    <code-header :text="header.text"/>
-                </template>
-                <template #item-player="{ player, team }">
-                    <div>
-                        <p>
-                            <router-link :to="to">
-                                {{ player }}
-                            </router-link>
-                        </p>
-                        <span>{{ team }}</span>
-                    </div>
-                </template>
-            </easy-data-table>
+            <table class="table table-hover">
+                <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">First</th>
+                    <th scope="col">Last</th>
+                    <th scope="col">Handle</th>
+                </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="(about, index) in abouts" :key="index">
+                        <th scope="row">1</th>
+                        <td>{{ about.ImagePath }}</td>
+                        <td>{{ about.Title }}</td>
+                        <td>{{ about.Slug }}</td>
+                        <td>{{ about.CreatedDate }}</td>
+                    </tr>
+                </tbody>
+            </table>
         </template>
     </index-content>
 </template>
@@ -70,81 +67,31 @@ export default {
 
     setup(props, context) {
         const itemsSelected = ref([]);
-        const tables = {
-            columns: [
-                {text: "PLAYER", value: "player"},
-                {text: "TEAM", value: "team"},
-                {text: "NUMBER", value: "number"},
-                {text: "POSITION", value: "position"},
-                {text: "HEIGHT", value: "indicator.height"},
-                {text: "WEIGHT (lbs)", value: "indicator.weight", sortable: true},
-                {text: "LAST ATTENDED", value: "lastAttended", width: 200},
-                {text: "COUNTRY", value: "country"},
-            ],
-            data: [
-                {
-                    player: "Stephen Curry",
-                    team: "GSW",
-                    number: 30,
-                    position: 'G',
-                    indicator: {"height": '6-2', "weight": 185},
-                    lastAttended: "Davidson",
-                    country: "USA"
-                },
-                {
-                    player: "Lebron James",
-                    team: "LAL",
-                    number: 6,
-                    position: 'F',
-                    indicator: {"height": '6-9', "weight": 250},
-                    lastAttended: "St. Vincent-St. Mary HS (OH)",
-                    country: "USA"
-                },
-                {
-                    player: "Kevin Durant",
-                    team: "BKN",
-                    number: 7,
-                    position: 'F',
-                    indicator: {"height": '6-10', "weight": 240},
-                    lastAttended: "Texas-Austin",
-                    country: "USA"
-                },
-                {
-                    player: "Giannis Antetokounmpo",
-                    team: "MIL",
-                    number: 34,
-                    position: 'F',
-                    indicator: {"height": '6-11', "weight": 242},
-                    lastAttended: "Filathlitikos",
-                    country: "Greece"
-                },
-            ]
-        };
-        const to = computed(() => {
-            return {name: Config.router.edit, params: {id: '123-123-123'}}
+        let abouts = ref([]);
+        service.actions.index().then(res => {
+            if (!isNull(res)) {
+                abouts.value = res.data.Data.Abouts.data;
+                // console.log(abouts[0].Title);
+            }
         })
-
-        onMounted(() => {
-            index();
-        })
-
-        const index = () => {
-            service.actions.index().then(res => {
-                if (!isNull(res)) {
-                    console.log(res);
-                }
-            })
-        }
+        // onMounted(() => {
+        //     index();
+        // })
+        //
+        // const index = () => {
+        //     service.actions.index().then(res => {
+        //         if (!isNull(res)) {
+        //             abouts.value = res.data.Data.Abouts.data;
+        //             // console.log(abouts[0].Title);
+        //         }
+        //     })
+        // }
 
         return {
-            tables,
             itemsSelected,
             Config,
-            to,
-            index
+            abouts
         }
-
-        console.log(index());
     },
 }
 </script>
